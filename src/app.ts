aac73b7705d31b3eea56beee65e79a1a773972c2
Sergey Grishchev <sergey@shadeapps.com>
2025-08-18 23:37:24 +0100

    Add MongoDB connection using mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.listen(PORT, () => console.log('Сервер работает! port: ' + PORT))
