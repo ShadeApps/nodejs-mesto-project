deb6714b20ac2713d5080a262dfd8c50c6a0a483
Sergey Grishchev <sergey@shadeapps.com>
2025-08-19 23:33:24 +0100

    Add MongoDB connection success and error handling

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import mongoose from 'mongoose';
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
-
-app.listen(PORT, () => console.log('Сервер работает! port: ' + PORT))
+mongoose.connect('mongodb://localhost:27017/mestodb')
+  .then(() => {
+    console.log('Успешное подключение к MongoDB.')
+    app.listen(PORT, () => console.log('Сервер работает. Port: ' + PORT));
+  })
+  .catch(err => console.error('Ошибка подключения к MongoDB:', err));
