265519aafd093b017d518e3531379e1dac3e233e
Sergey Grishchev <sergey@shadeapps.com>
2025-08-22 16:23:34 +0100

    Extend cards controller

---
+++ src/controllers/cards.ts
@@ -0,0 +1,111 @@
+import { Request, Response } from 'express';
+import Card from '../models/card';
+import '../types/express';
+
+export const createCard = async (req: Request, res: Response) => {
+  try {
+    const {
+      name, link, owner = req.user._id, likes, createdAt,
+    } = req.body;
+    const card = await Card.create({
+      name, link, owner, likes, createdAt,
+    });
+    return res.status(201).json(card);
+  } catch (error: any) {
+    if (error.name === 'ValidationError') {
+      return res.status(400).json({
+        message: 'Переданы некорректные данные при создании карточки.',
+      });
+    }
+    return res.status(500).json(error);
+  }
+};
+
+export const getAllCards = async (req: Request, res: Response) => {
+  try {
+    const cards = await Card.find();
+    return res.status(200).json(cards);
+  } catch (error: any) {
+    return res.status(500).json(error);
+  }
+};
+
+export const deleteCard = async (req: Request, res: Response) => {
+  try {
+    const card = await Card.findByIdAndDelete(req.params.cardId);
+    if (!card) {
+      return res.status(404).json({
+        message: 'Карточка с указанным _id не найдена.',
+      });
+    }
+    return res.status(200).json(card);
+  } catch (error: any) {
+    if (error.name === 'SyntaxError') {
+      return res.status(400).json({
+        message: 'Передан некорректный _id карточки.',
+      });
+    }
+    if (error.name === 'CastError') {
+      return res.status(404).json({
+        message: 'Карточка с указанным _id не найдена.',
+      });
+    }
+    return res.status(500).json(error);
+  }
+};
+
+export const addLike = async (req: Request, res: Response) => {
+  try {
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } } as any,
+      { new: true },
+    );
+    if (!card) {
+      return res.status(404).json({
+        message: 'Передан несуществующий _id карточки.',
+      });
+    }
+    return res.status(200).json(card);
+  } catch (error: any) {
+    if (error.name === 'SyntaxError') {
+      return res.status(400).json({
+        message: 'Переданы некорректные данные для постановки/снятия лайка или некорректный _id карточки.',
+      });
+    }
+    if (error.name === 'CastError') {
+      return res.status(400).json({
+        message: 'Переданы некорректные данные для постановки/снятия лайка или некорректный _id карточки.',
+      });
+    }
+    return res.status(500).json(error);
+  }
+};
+
+export const removeLike = async (req: Request, res: Response) => {
+  try {
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } } as any,
+      { new: true },
+    );
+    if (!card) {
+      return res.status(404).json({
+        message: 'Передан несуществующий _id карточки.',
+      });
+    }
+    return res.status(200).json(card);
+  } catch (error: any) {
+    if (error.name === 'SyntaxError') {
+      return res.status(400).json({
+        message: 'Переданы некорректные данные для постановки/снятия лайка или некорректный _id карточки.',
+      });
+    }
+    if (error.name === 'CastError') {
+      return res.status(400).json({
+        message: 'Переданы некорректные данные для постановки/снятия лайка или некорректный _id карточки.',
+      });
+    }
+    return res.status(500).json(error);
+  }
+};
