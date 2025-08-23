49586ee94d7e359bc0567eea2253c6e270b9004e
Sergey Grishchev <sergey@shadeapps.com>
2025-08-23 14:34:06 +0100

    Rename to types

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/users';
 import cardRouter from './routes/cards';
-import './types/express';
+import './utils/types';
 
 const { PORT = 3000 } = process.env;
 const app = express();
