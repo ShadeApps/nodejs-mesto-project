c8c0e796abf670599844d88b5a90ff2f85d44411
Sergey Grishchev <sergey@shadeapps.com>
2025-08-24 00:57:36 +0100

    Use user validation

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import {
   getAllUsers, getUserById, updateUserProfile, updateUserAvatar, getCurrentUser,
 } from '../controllers/users';
+import { validateUpdateUser, validateUserId, validateUpdateUserAvatar } from '../middlewares/validators';
 
 const usersRouter = Router();
 
 usersRouter.get('/users', getAllUsers);
-usersRouter.get('/users/:userId', getUserById);
+usersRouter.get('/users/:userId', validateUserId, getUserById);
 usersRouter.get('/users/me', getCurrentUser);
-usersRouter.patch('/users/me', updateUserProfile);
-usersRouter.patch('/users/me/avatar', updateUserAvatar);
+usersRouter.patch('/users/me', validateUpdateUser, updateUserProfile);
+usersRouter.patch('/users/me/avatar', validateUpdateUserAvatar, updateUserAvatar);
 
 export default usersRouter;
