d236790ed3a5e968a5b0caeaf17da2a9fcb730e9
Sergey Grishchev <sergey@shadeapps.com>
2025-08-24 14:48:17 +0100

    Fix users/me endpoint

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -7,8 +7,8 @@ import { validateUpdateUser, validateUserId, validateUpdateUserAvatar } from '..
 const usersRouter = Router();
 
 usersRouter.get('/users', getAllUsers);
-usersRouter.get('/users/:userId', validateUserId, getUserById);
 usersRouter.get('/users/me', getCurrentUser);
+usersRouter.get('/users/:userId', validateUserId, getUserById);
 usersRouter.patch('/users/me', validateUpdateUser, updateUserProfile);
 usersRouter.patch('/users/me/avatar', validateUpdateUserAvatar, updateUserAvatar);
 
