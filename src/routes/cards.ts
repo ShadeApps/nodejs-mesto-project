9c32f9b0000e51d655e45b7553010b694d2c7b8f
Sergey Grishchev <sergey@shadeapps.com>
2025-08-24 01:05:39 +0100

    Add validation middleware to cards routes

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import {
   getAllCards, createCard, deleteCard, addLike, deleteLike,
 } from '../controllers/cards';
+import { validateCardId, validateCreateCard } from '../middlewares/validators';
 
 const cardsRouter = Router();
 
 cardsRouter.get('/cards', getAllCards);
-cardsRouter.post('/cards', createCard);
-cardsRouter.delete('/cards/:cardId', deleteCard);
-cardsRouter.put('/cards/:cardId/likes', addLike);
-cardsRouter.delete('/cards/:cardId/likes', deleteLike);
+cardsRouter.post('/cards', validateCreateCard, createCard);
+cardsRouter.delete('/cards/:cardId', validateCardId, deleteCard);
+cardsRouter.put('/cards/:cardId/likes', validateCardId, addLike);
+cardsRouter.delete('/cards/:cardId/likes', validateCardId, deleteLike);
 
 export default cardsRouter;
