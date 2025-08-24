2920a9b39cd17a01afb15e187c1df78ebc89c5a7
Sergey Grishchev <sergey@shadeapps.com>
2025-08-24 01:13:09 +0100

    Add request and error logging middleware

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import userRouter from './routes/users';
 import cardRouter from './routes/cards';
-import auth from './middlewares/auth';import errorHandler from './middlewares/errorHandler';
+import auth from './middlewares/auth';
+import errorHandler from './middlewares/errorHandler';
 import { requestLogger, errorLogger } from './middlewares/logger';
 import { validateSignIn, validateSignUp } from './middlewares/validators';
 import { login, createUser } from './controllers/users';
@@ -17,6 +18,8 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+app.use(requestLogger);
+
 app.post('/signup', validateSignUp, createUser);
 app.post('/signin', validateSignIn, login);
 
@@ -28,7 +31,9 @@ app.use('*', (req: Request, res: Response, next: NextFunction) => {
   next(new AppError('Тут ничего нет.', 404));
 });
 
+app.use(errorLogger);
 app.use(errors());
+app.use(errorHandler);
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
